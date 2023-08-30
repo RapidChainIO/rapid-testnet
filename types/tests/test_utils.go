38846ceb49a21c80744406c88895cc2739ac4c91
SinyorDes <sinyordesyt@gmail.com>
2023-08-30 19:13:52 +0300

    rapid-chain

---
+++ types/tests/test_utils.go
@@ -0,0 +1,40 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package tests
+
+import (
+	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+)
+
+var (
+	UosmoDenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-0",
+		BaseDenom: "uosmo",
+	}
+	UosmoIbcdenom = UosmoDenomtrace.IBCDenom()
+
+	UatomDenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-1",
+		BaseDenom: "uatom",
+	}
+	UatomIbcdenom = UatomDenomtrace.IBCDenom()
+
+	Urapiddenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-0",
+		BaseDenom: "arapid",
+	}
+	UevmosIbcdenom = Urapiddenomtrace.IBCDenom()
+
+	UatomOsmoDenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-0/transfer/channel-1",
+		BaseDenom: "uatom",
+	}
+	UatomOsmoIbcdenom = UatomOsmoDenomtrace.IBCDenom()
+
+	Arapiddenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-0",
+		BaseDenom: "arapid",
+	}
+	arapidIbcdenom = Arapiddenomtrace.IBCDenom()
+)
